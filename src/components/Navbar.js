4da1ebc62696d0e09753317d28fbd65d2ca5f1c6
Eluro <elurolydia@gmail.com>
2021-11-26 16:14:35 +0100

    added href to anchor tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <div className ='Navbar'>
             <ul>
-                <li className="bold"><a href="#">Home</a></li>
-                <li><a href="#">Gallery</a></li>
-                <li><a href="#">About us</a></li>
-                <li><a href="#">Contact us</a></li>
+                <li className="bold"><a href="">Home</a></li>
+                <li><a href="">Gallery</a></li>
+                <li><a href="">About us</a></li>
+                <li><a href="">Contact us</a></li>
             </ul>
         </div>
     )

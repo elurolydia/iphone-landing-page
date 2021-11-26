a56aba77ec2235f9a7cb2b1239dc38018ec12313
Eluro <elurolydia@gmail.com>
2021-11-26 15:47:07 +0100

    fixed warnings

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
         <div className ='Navbar'>
             <ul>
-                <li className="bold"><a>Home</a></li>
-                <li><a>Gallery</a></li>
-                <li><a>About us</a></li>
-                <li><a>Contact us</a></li>
+                <li className="bold"><a href="#">Home</a></li>
+                <li><a href="#">Gallery</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Contact us</a></li>
             </ul>
         </div>
     )

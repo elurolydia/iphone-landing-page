8ff8670c38c9f4360d4d17708a51f9d767ef77ab
Eluro <elurolydia@gmail.com>
2021-11-27 00:39:25 +0100

    added check for desktop screens only

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Phones from './components/Phones';
+import ScreenSizeError from './components/PhoneScreenError';
 
 
 function App() {
+  const isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+
+  if (isMobile){
+    return (
+      <div className="error_parent">
+        <ScreenSizeError/>
+      </div>
+    );
+  }
   return (
     <div>
       <Navbar/>

d0254add2f70793d5d210cc05d1be09e09b71066
Eluro <elurolydia@gmail.com>
2021-11-26 14:27:01 +0100

    Updates

---
+++ precache-manifest.82995c00627c11dbc02d8b2f7785a94d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da4dd818e0f78ea889e0391c1cebc4c",
+    "url": "/elurolydia/iphone-landing-page/index.html"
+  },
+  {
+    "revision": "037946f3b390a914496a",
+    "url": "/elurolydia/iphone-landing-page/static/css/main.8b673079.chunk.css"
+  },
+  {
+    "revision": "a47aac1ccb92cf244a1c",
+    "url": "/elurolydia/iphone-landing-page/static/js/2.392adf55.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/elurolydia/iphone-landing-page/static/js/2.392adf55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "deab8be98d5aeec348ab",
+    "url": "/elurolydia/iphone-landing-page/static/js/3.86d2f770.chunk.js"
+  },
+  {
+    "revision": "037946f3b390a914496a",
+    "url": "/elurolydia/iphone-landing-page/static/js/main.110d26c2.chunk.js"
+  },
+  {
+    "revision": "42ba859ebac564e4aef0",
+    "url": "/elurolydia/iphone-landing-page/static/js/runtime-main.5e85faf2.js"
+  },
+  {
+    "revision": "ae28fbd3e236f51aebbeffe84595623a",
+    "url": "/elurolydia/iphone-landing-page/static/media/iPhones.ae28fbd3.png"
+  }
+]);
